ce86e0f84305a0dd2467705f0bbc601aa1f3472a
arlo-engineer <yoshikawa.engineer@gmail.com>
2024-02-27 20:46:23 +0900

    リロードした時にOAuth認証が剥がれるエラーを解決
    $client->getAccessTokenがリロードによりNULLとなってしまうが、$_SESSIONで解決

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 // セットアップ
 require_once __DIR__ . '/vendor/autoload.php';
+session_start();
 
 // クライアントIDと秘密鍵を設定
 $client_id = "999582628275-jdbma12tpptu3bja12v1o408j98qo156.apps.googleusercontent.com";
@@ -15,27 +16,35 @@ $client->setClientId($client_id);
 $client->setClientSecret($client_secret);
 $client->setRedirectUri($redirect_uri);
 $client->setScopes(['https://www.googleapis.com/auth/youtube.readonly']);
+$client->setPrompt('consent');
 
 // アクセストークンが設定されていない場合、認証URLを生成
 if (! isset($_GET['code'])) {
 $auth_url = $client->createAuthUrl();
 header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 } else {
-// アクセストークンを取得
-$client->authenticate($_GET['code']);
-$access_token = $client->getAccessToken();
+// リロード時にアクセストークンがセッションにあればそれを復元
+    if (isset($_SESSION['access_token'])) {
+        $client->setAccessToken($_SESSION['access_token']);
+    }
+    // アクセストークンを取得->else以降が変でLaravelの方でも同様のエラーが発生していると思われる
+    $auth_url = $client->createAuthUrl();
+    $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $access_token = $client->getAccessToken(); // 一度目のアクセスの場合は中身が存在しているが、リロードするとNULLとなる。
+    if (isset($access_token)) {
+        $_SESSION['access_token'] = $access_token;
+    }
+    $client->setScopes(['https://www.googleapis.com/auth/youtube.readonly']);
 
-// YouTube Data APIにリクエストを送信
-$youtube = new Google_Service_YouTube($client);
-$response = $youtube->subscriptions->listSubscriptions('snippet,contentDetails,subscriberSnippet', array("mine" => "true"));
+    // YouTube Data APIにリクエストを送信
+    $youtube = new Google_Service_YouTube($client);
+    $response = $youtube->subscriptions->listSubscriptions('snippet,contentDetails,subscriberSnippet', array("mine" => "true"));
 
-// 登録チャンネル名を取得
-foreach ($response['items'] as $result) {
-    $titles = $result['snippet']['title'];
-    echo '<pre>';
-    echo $titles;
-    echo '<pre>';
+    // 登録チャンネル名を表示
+    foreach ($response['items'] as $result) {
+        $titles = $result['snippet']['title'];
+        echo '<pre>';
+        echo $titles;
+        echo '<pre>';
+    }
 }
-
-};
-
